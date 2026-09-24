d792eb3bd516e6e138c8c739df37b57fdf99fa5a
mattw1285 <mattw1285@gmail.com>
2026-09-24 20:39:48 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const map = L.map('map').setView([54.5,-2.5],6);
 
 L.tileLayer(
-    'https://tile.openstreetmap.org/{z}/{y}/{x}.png',
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
         maxZoom:19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'     
